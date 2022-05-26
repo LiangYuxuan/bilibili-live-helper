41465a804fe4d649f06cb3d914e4275f54d03368
LiangYuxuan <lyx@lyx.es>
2022-05-26 17:15:24 +0800

    feat: new tool to allow users fetch cookies by qrcode login

--- README.md
+++ README.md
@@ -17,16 +17,16 @@ Bilibili直播相关自动化脚本
 - [x] 粉丝勋章弹幕打卡
 - [x] 自动赠送背包礼物
 - [x] 自动点赞直播间3次
-- [x] 自动分享直播间5次
+- [x] 自动分享直播间5次（并不会实际分享）
 - [x] 自动观看直播间30分钟
 
 ## 配置方法
 
-1. 运行环境
+### 1. 运行环境
 
 需要环境 Node.js >= 14.18.2
 
-2. 配置
+### 2. 配置
 
 ```bash
 cp .env.example .env
@@ -35,9 +35,11 @@ vi .env
 
 根据注释修改，如果需要禁用某项功能，将等号后置空或者改为0。
 
-3. 获取Cookies
+### 3. 获取Cookies
 
 程序会尝试从以下途径获取Cookies：`.env`中的`COOKIES`、`.cookies`文件内容、环境变量`COOKIES`。
+
+#### 3.1. 使用现代浏览器完成
 
 打开无痕模式，随便打开一个直播间，然后打开开发人员工具，在网络/Network选项卡内找到`heartBeat`或`webHeartBeat`请求，在请求头中找到Cookie，复制冒号后面的内容（即下图浅蓝色部分）。
 
@@ -45,7 +47,24 @@ vi .env
 
 请确认Cookies里存在`bili_jct=`和`LIVE_BUVID=`。
 
-4. 开始运行
+#### 3.2. 使用自带工具完成
+
+```bash
+npm install
+npm run dev:cookies
+```
+
+或者
+
+```bash
+npm install
+npm run build
+node dist/cookies.js
+```
+
+使用手机客户端扫描显示的二维码完成登录后，工具将自动将Cookies写入.cookies文件。
+
+### 4. 开始运行
 
 ```bash
 npm install
---
+++ src/cookies.ts
@@ -0,0 +1,135 @@
+/* eslint-disable camelcase */
+
+import assert from 'assert';
+import fs from 'fs';
+import got from 'got';
+import qrcode from 'qrcode-terminal';
+
+const UserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36';
+
+interface LoginUrlData {
+    code: number;
+    status: boolean;
+    ts: number;
+    data: {
+        url: string;
+        oauthKey: string;
+    };
+}
+
+const getLoginUrl = async () => {
+    const result: LoginUrlData = await got.get('https://passport.bilibili.com/qrcode/getLoginUrl', {
+        headers: {
+            'User-Agent': UserAgent,
+            'Referer': 'https://passport.bilibili.com/',
+        },
+    }).json();
+
+    assert(result.code === 0, '获取登录二维码失败');
+
+    return result.data;
+};
+
+interface LoginInfoData {
+    code?: number;
+    message?: string;
+    ts?: number;
+    status: boolean;
+    data: {
+        url: string;
+    } | number;
+}
+
+const getLoginInfo = async (oauthKey: string) => {
+    const result: LoginInfoData = await got.post('https://passport.bilibili.com/qrcode/getLoginInfo', {
+        headers: {
+            'User-Agent': UserAgent,
+            'Referer': 'https://passport.bilibili.com/',
+        },
+        form: {
+            oauthKey,
+        },
+    }).json();
+
+    return result;
+};
+
+const checkLogin = async (oauthKey: string) => {
+    const startTime = Date.now();
+
+    while (Date.now() - startTime < 180 * 1000) {
+        const {status, data} = await getLoginInfo(oauthKey);
+
+        if (status && typeof data === 'object') {
+            return data.url;
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+    }
+
+    throw new Error('二维码扫描超时');
+};
+
+(async () => {
+    const {url: qrcodeContent, oauthKey} = await getLoginUrl();
+
+    qrcode.generate(qrcodeContent, {
+        small: true,
+    });
+
+    const url = await checkLogin(oauthKey);
+
+    await got.get(url, {
+        headers: {
+            'User-Agent': UserAgent,
+            'Referer': 'https://passport.bilibili.com/',
+        },
+    });
+
+    let DedeUserID = url.match(/DedeUserID=([^&]+)/)?.[1];
+    let DedeUserID__ckMd5 = url.match(/DedeUserID__ckMd5=([^&]+)/)?.[1];
+    let SESSDATA = url.match(/SESSDATA=([^&]+)/)?.[1];
+    let bili_jct = url.match(/bili_jct=([^&]+)/)?.[1];
+
+    assert(DedeUserID, '解析登录URL失败: 未找到DedeUserID');
+    assert(DedeUserID__ckMd5, '解析登录URL失败: 未找到DedeUserID__ckMd5');
+    assert(SESSDATA, '解析登录URL失败: 未找到SESSDATA');
+    assert(bili_jct, '解析登录URL失败: 未找到bili_jct');
+
+    DedeUserID = decodeURI(DedeUserID);
+    DedeUserID__ckMd5 = decodeURI(DedeUserID__ckMd5);
+    SESSDATA = decodeURI(SESSDATA);
+    bili_jct = decodeURI(bili_jct);
+
+    let cookies = `DedeUserID=${DedeUserID}; DedeUserID__ckMd5=${DedeUserID__ckMd5}; SESSDATA=${SESSDATA}; bili_jct=${bili_jct}`;
+
+    const request = await got.get('https://api.live.bilibili.com/gift/v3/live/gift_config', {
+        headers: {
+            'User-Agent': UserAgent,
+            'Referer': 'https://live.bilibili.com/',
+            'Cookie': cookies,
+        },
+    });
+
+    const header = request.headers['set-cookie'];
+
+    assert(header, '获取LIVE_BUVID失败: 未找到Set-Cookie头');
+
+    let LIVE_BUVID: string | undefined;
+
+    header.forEach((text) => {
+        const match = text.match(/LIVE_BUVID=([^;]+)/);
+
+        if (match) {
+            LIVE_BUVID = match[1];
+        }
+    });
+
+    assert(LIVE_BUVID, '获取LIVE_BUVID失败: 未找到LIVE_BUVID');
+
+    cookies = `${cookies}; LIVE_BUVID=${LIVE_BUVID}`;
+
+    fs.writeFileSync('.cookies', cookies);
+
+    console.log('获取Cookies成功，已经写入.cookies文件。');
+})().catch(console.log);

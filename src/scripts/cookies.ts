8cc2d5e0a9846f23cdcabf5a5a5f7f8edff16195
LiangYuxuan <lyx@lyx.es>
2024-02-26 11:30:54 +0800

    feat: use new api to login

--- src/scripts/cookies.ts
+++ src/scripts/cookies.ts
@@ -11,20 +11,20 @@ import { delay } from '../utils.ts';
 const UserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36';
 
 interface LoginUrlData {
-    code: number;
-    status: boolean;
-    ts: number;
+    code: number,
+    message: boolean,
+    ttl: number,
     data: {
-        url: string;
-        oauthKey: string;
-    };
+        url: string,
+        qrcode_key: string,
+    },
 }
 
 const getLoginUrl = async () => {
-    const result: LoginUrlData = await got.get('https://passport.bilibili.com/qrcode/getLoginUrl', {
+    const result: LoginUrlData = await got.get('https://passport.bilibili.com/x/passport-login/web/qrcode/generate', {
         headers: {
             'User-Agent': UserAgent,
-            Referer: 'https://passport.bilibili.com/',
+            Referer: 'https://www.bilibili.com/',
         },
     }).json();
 
@@ -34,49 +34,87 @@ const getLoginUrl = async () => {
 };
 
 interface LoginInfoData {
-    code?: number;
-    message?: string;
-    ts?: number;
-    status: boolean;
+    code: number,
+    message: string,
+    ttl: number,
     data: {
-        url: string;
-    } | number;
+        url: '',
+        refresh_token: '',
+        timestamp: 0,
+        code: 86101,
+        message: '未扫码',
+    } | {
+        url: '',
+        refresh_token: '',
+        timestamp: 0,
+        code: 86038,
+        message: '二维码已失效',
+    } | {
+        url: '',
+        refresh_token: '',
+        timestamp: 0,
+        code: 86090,
+        message: '二维码已扫码未确认',
+    } | {
+        url: string,
+        refresh_token: string,
+        timestamp: number,
+        code: 0,
+        message: '',
+    },
 }
 
-const getLoginInfo = async (oauthKey: string) => {
-    const result: LoginInfoData = await got.post('https://passport.bilibili.com/qrcode/getLoginInfo', {
+const getLoginInfo = async (qrcode_key: string) => {
+    const res = await got.get('https://passport.bilibili.com/x/passport-login/web/qrcode/poll', {
         headers: {
             'User-Agent': UserAgent,
-            Referer: 'https://passport.bilibili.com/',
+            Referer: 'https://www.bilibili.com/',
         },
-        form: {
-            oauthKey,
+        searchParams: {
+            qrcode_key,
         },
-    }).json();
+    });
 
-    return result;
+    const cookies = res.headers['set-cookie']?.map((text: string) => text.split(';')[0].split('=')) as [string, string][] | undefined;
+    const result = JSON.parse(res.body) as LoginInfoData;
+
+    return {
+        ...result,
+        cookies,
+    };
 };
 
-const checkLogin = async (oauthKey: string) => {
+const checkLogin = async (qrcode_key: string) => {
     const startTime = Date.now();
+    let isScanned = false;
 
-    while (Date.now() - startTime < 180 * 1000) {
+    while (Date.now() - startTime < 3 * 60 * 1000) {
         // eslint-disable-next-line no-await-in-loop
-        const { status, data } = await getLoginInfo(oauthKey);
+        const { data, cookies } = await getLoginInfo(qrcode_key);
 
-        if (status && typeof data === 'object') {
-            return data.url;
+        if (data.code === 0) {
+            return new Map(cookies ?? []);
+        }
+
+        if (data.code === 86038) {
+            break;
+        }
+
+        if (data.code === 86090 && !isScanned) {
+            console.log('二维码已扫描');
+            isScanned = true;
         }
 
         // eslint-disable-next-line no-await-in-loop
         await delay(2000);
     }
 
-    throw new Error('二维码扫描超时');
+    console.error('二维码已失效');
+    throw new Error('二维码已失效');
 };
 
 (async () => {
-    const { url: qrcodeContent, oauthKey } = await getLoginUrl();
+    const { url: qrcodeContent, qrcode_key } = await getLoginUrl();
 
     qrcode.generate(qrcodeContent, {
         small: true,
@@ -84,61 +122,35 @@ const checkLogin = async (oauthKey: string) => {
 
     console.log('请使用哔哩哔哩手机客户端扫描二维码登录');
 
-    const url = await checkLogin(oauthKey);
-
-    await got.get(url, {
-        headers: {
-            'User-Agent': UserAgent,
-            Referer: 'https://passport.bilibili.com/',
-        },
-    });
+    const cookies = await checkLogin(qrcode_key);
 
     console.log('登录成功，正在获取获取LIVE_BUVID...');
 
-    let DedeUserID = url.match(/DedeUserID=([^&]+)/)?.[1];
-    let DedeUserID__ckMd5 = url.match(/DedeUserID__ckMd5=([^&]+)/)?.[1];
-    let SESSDATA = url.match(/SESSDATA=([^&]+)/)?.[1];
-    let bili_jct = url.match(/bili_jct=([^&]+)/)?.[1];
+    const DedeUserID = cookies.get('DedeUserID');
+    const DedeUserID__ckMd5 = cookies.get('DedeUserID__ckMd5');
+    const SESSDATA = cookies.get('SESSDATA');
+    const bili_jct = cookies.get('bili_jct');
 
-    assert(DedeUserID, '解析登录URL失败: 未找到DedeUserID');
-    assert(DedeUserID__ckMd5, '解析登录URL失败: 未找到DedeUserID__ckMd5');
-    assert(SESSDATA, '解析登录URL失败: 未找到SESSDATA');
-    assert(bili_jct, '解析登录URL失败: 未找到bili_jct');
+    assert(DedeUserID, '获取Cookies失败: 未找到DedeUserID');
+    assert(DedeUserID__ckMd5, '获取Cookies失败: 未找到DedeUserID__ckMd5');
+    assert(SESSDATA, '获取Cookies失败: 未找到SESSDATA');
+    assert(bili_jct, '获取Cookies失败: 未找到bili_jct');
 
-    DedeUserID = decodeURI(DedeUserID);
-    DedeUserID__ckMd5 = decodeURI(DedeUserID__ckMd5);
-    SESSDATA = decodeURI(SESSDATA);
-    bili_jct = decodeURI(bili_jct);
-
-    let cookies = `DedeUserID=${DedeUserID}; DedeUserID__ckMd5=${DedeUserID__ckMd5}; SESSDATA=${SESSDATA}; bili_jct=${bili_jct}`;
+    const cookiesText = `DedeUserID=${DedeUserID}; DedeUserID__ckMd5=${DedeUserID__ckMd5}; SESSDATA=${SESSDATA}; bili_jct=${bili_jct}`;
 
     const request = await got.get('https://api.live.bilibili.com/gift/v3/live/gift_config', {
         headers: {
             'User-Agent': UserAgent,
             Referer: 'https://live.bilibili.com/',
-            Cookie: cookies,
+            Cookie: cookiesText,
         },
     });
 
-    const header = request.headers['set-cookie'];
-
-    assert(header, '获取LIVE_BUVID失败: 未找到Set-Cookie头');
-
-    let LIVE_BUVID: string | undefined;
-
-    header.forEach((text) => {
-        const match = text.match(/LIVE_BUVID=([^;]+)/);
-
-        if (match) {
-            [LIVE_BUVID] = match;
-        }
-    });
+    const LIVE_BUVID = request.headers['set-cookie']?.find((text: string) => text.startsWith('LIVE_BUVID'))?.split(';')[0];
 
     assert(LIVE_BUVID, '获取LIVE_BUVID失败: 未找到LIVE_BUVID');
 
-    cookies = `${cookies}; LIVE_BUVID=${LIVE_BUVID}`;
-
-    fs.writeFileSync('.cookies', cookies);
+    fs.writeFileSync('.cookies', `${cookiesText}; ${LIVE_BUVID}`);
 
     console.log('获取Cookies成功，已经写入.cookies文件。');
 })().catch(console.log);
